3f740aa9f69841b433f2e2b3b77d6ee84d4e7cdd
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-06-04 20:58:12 +0900

    update

--- docs/components/sampleSketch.js
+++ docs/components/sampleSketch.js
@@ -1,5 +1,7 @@
 import p5 from 'p5';
-console.log(p5);
+//console.log(p5);
+
+
 
 const sketch = (p) => {
   p.setup = () => {
@@ -11,6 +13,30 @@ const sketch = (p) => {
     p.ellipse(50, 50, 80, 80);
   };
 };
+
+
+
+export default function mount(container) {
+  // 1. 渡されたコンテナの中にHTMLを展開
+  
+  //appendChild
+  container.innerHTML = `
+    <div style="padding: 1rem; border: 1px solid #ccc; border-radius: 8px;">
+      <p>このコンポーネントはJSから生成されています</p>
+      <button class="action-btn">クリック!</button>
+    </div>
+  `;
+
+  // 2. イベントリスナーの設定
+  // 🌟 重要: document 全体ではなく、自分の container の中だけを探せばよい!
+  // だから IDの衝突 (UUID問題) を気にする必要がありません。
+  const btn = container.querySelector('.action-btn');
+  btn.addEventListener('click', () => {
+    alert('JSファイルからマウントされたボタンです!');
+  });
+}
+
+
 
 const p5sketch = new p5(sketch);
 
