5b3e8ea4b1084292c19d7bc620ad4b1e9bbb234e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-06-14 11:38:17 +0900

    up

--- docs/content/sketches/s0_00_00_a.js
+++ docs/content/sketches/s0_00_00_a.js
@@ -1,7 +1,7 @@
 export const sketch = (p) => {
   p.setup = () => {
-    //p.createCanvas(2000, 2000, p.WEBGL);
-    p.createCanvas(720, 720, p.WEBGL);
+    p.createCanvas(2000, 2000, p.WEBGL);
+
     p.background(150);
     p.stroke(0, 50);
     p.fill(255, 200);
@@ -9,29 +9,27 @@ export const sketch = (p) => {
     const xstart = p.random(10);
     let ynoise = p.random(10);
 
-    for (let y = -(p.height / 8); y < p.height / 8; y += 3) {
+    p.translate(0, 0, 0);
+
+    for (let y = -(p.height / 8); y <= p.height / 8; y += 3) {
       ynoise += 0.02;
       let xnoise = xstart;
-      for (let x = -(p.width / 8); x < p.width / 8; x += 3) {
+
+      for (let x = -(p.width / 8); x <= p.width / 8; x += 3) {
         xnoise += 0.02;
         drawPoint(x, y, p.noise(xnoise, ynoise));
       }
     }
-
-    p.noLoop();
   };
 
-  p.draw = () => {};
-
-  function drawPoint(x, y, noiseFactor) {
+  const drawPoint = (x, y, noiseFactor) => {
     p.push();
-    //p.translate(-w/2, -h/2, 0);
+
     p.translate(x * noiseFactor * 4, y * noiseFactor * 4, -y);
-    //p.translate(w/2, h/2, 0);
-    //p.translate(noiseFactor * 4, noiseFactor * 4, -y);
+
     const edgeSize = noiseFactor * 26;
-    //p.ellipse(x, y, edgeSize, edgeSize);
     p.ellipse(0, 0, edgeSize, edgeSize);
+
     p.pop();
-  }
+  };
 };
